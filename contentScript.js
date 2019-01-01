f32c0d9099899b4fb62ba11a043333da3c471d82
hon_ya <spaceodyssey2008@gmail.com>
2019-01-01 17:29:10 +0900

    youtube トップ画面からチャンネル画面に遷移したときにリンクが生成されない問題を修正

--- contentScript.js
+++ contentScript.js
@@ -1,23 +1,42 @@
 const createElementFromHTML = htmlString => {
-  var e = document.createElement('div');
+  var e = document.createElement("div");
   e.innerHTML = htmlString.trim();
   return e.firstChild; 
 };
 
 const init = async() => {
 
-  let liveUrlStr = `
-    <div style="font-size: 16px; width: 36px; height: 24px; margin: 10px 0px;">
-      <a id="yll-link" href="${location.pathname}/live">/live</a>
-    </div>
-  `;
-  let liveUrl = createElementFromHTML(liveUrlStr);
+  let addOrUpdate = () => {
+    // URL をチェックし、channel 以下の場合にリンクの追加および更新を行う
+    if (/^\/channel\/.+/.test(location.pathname)) {
 
-  let buttonRenderer = document.querySelector('ytd-subscribe-button-renderer');
-  buttonRenderer.appendChild(liveUrl);
+      Array.prototype.slice.call(document.querySelectorAll("ytd-subscribe-button-renderer")).forEach((buttonRenderer) => {
+        if (buttonRenderer.className === "style-scope ytd-c4-tabbed-header-renderer") {
+          let link = buttonRenderer.querySelector("#yll-link");
+          if (link == null) {
+            // 追加
+            let blockStr = `
+              <div id="yll-block" style="font-size: 16px; width: 36px; height: 24px; margin: 10px 0px;">
+                <a id="yll-link" href="${location.pathname}/live">/live</a>
+              </div>
+            `;
+            let blockHtml = createElementFromHTML(blockStr);
+            buttonRenderer.appendChild(blockHtml);
+          } else {
+            // 更新
+            link.href = `${location.pathname}/live`;
+          }
+        }
+      });
+    }
+  }
 
-  (document.body || document.documentElement).addEventListener('transitionend', () => {
-    liveUrl.querySelector("#yll-link").href = `${location.pathname}/live`;
+  // スクリプト初回実行時にとりあえず追加を試みる
+  addOrUpdate();
+
+  (document.body || document.documentElement).addEventListener("transitionend", () => {
+    // チャンネルページ移動が行われる場合に追加あるいは更新を試みる
+    addOrUpdate();
   });
 };
 init();
